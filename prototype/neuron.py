c7692b119321f839c872432fc396abce8d269691
Ian Patrick M Tan <ianpatricktan@gmail.com>
2022-02-15 13:27:13 -0500

    added comments and cleaned up a bit

--- prototype/neuron.py
+++ prototype/neuron.py
@@ -1,3 +1,7 @@
+"""
+This is a prototype model covering only a single neuron and has no synaptic plasticity capabilities. 
+"""
+
 import numpy as np
 
 
@@ -16,16 +20,16 @@ class Neuron:
         self.cationInflux = 0
 
     def evolve(self, inputs):
-        self.potential -= self.cationInflux
+        # cationInflux is the amount of cations that flowed in this iteration, it carries a fraction of the previous iteration's influx with a bit added decided by the cation parameter and the potential of the neuron
         self.cationInflux = self.cationInflux * 0.6 + self.cation * self.potential
-        print('cation influx:', self.cationInflux)
+        self.potential -= self.cationInflux
+        # anionInflux is the amount of anions that flowed in this iteration and is decided by the sum of the input weight products along with a bit more that's dependent on the neuron's potential
         if not self.refractory:
             anionInflux = np.sum(inputs * self.weights) / self.synapseQuantity + self.anion * self.potential
             self.potential += anionInflux * (self.potential + anionInflux > 0)
+        # this decides the absolute refractory period, as the neuron depolarizes it will reach a threshold which will trigger the absolute refractory period which will block all anion intake from that point until the absolute refractory period ends which is when it passes another threshold and enters the relative refractory period
         if self.refractory == False and self.potential >= self.depolarizationThreshold:
-            print('\nRefractory period has started\n')
             self.refractory = True
         if self.refractory == True and self.potential <= self.refractoryThreshold:
-            print('\nRefractory period has ended\n')
             self.refractory = False
 
