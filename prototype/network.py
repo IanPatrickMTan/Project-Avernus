27cbe41ee149c4e3134d0ca14982850915b9abdf
Ian Patrick M Tan <ianpatricktan@gmail.com>
2022-02-19 08:09:25 -0500

    started converting neuron code to work on the entirety of the network

--- prototype/network.py
+++ prototype/network.py
@@ -4,5 +4,21 @@ import numpy as np
 class Network
 
 
-    def __init__(self):
+    def __init__(self, weights, potentials, cation, anion, depolThresh, refractThresh, velDec):
+        self.weights = weights
+        self.potentials = potentials
+        self.cation = cation
+        self.anion = anion
+        self.depolThresh = depolThresh
+        self.refractThresh = refractThresh
+        self.velDec = velDec
+        self.absRefract = np.full(len(weights), False)
+        self.cationIn = np.zeros(len(weights[0]))
 
+    def evolve(self, inputs):
+        self.cationIn = self.cationIn * self.velDec + self.action * self.potentials
+        self.potentials -= self.cationIn
+
+        depoling = np.where(absRefract)
+        anionIn = np.sum(inputs[depoling] * self.weights[depoling]) / len(self.weights[0]) + self.anion * self.potentials[depoling]
+        self.potentials[np.where(absRefract and )]
